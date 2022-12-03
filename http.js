1a552cc81b3e1461becc62000cdf377c541e0ec2
spornov91 <m79513829426@gmail.com>
2022-12-03 18:20:35 +0700

    add http routing

---
+++ http.js
@@ -0,0 +1,43 @@
+const path = require('path') 
+const fs = require('fs') 
+
+const option = { 
+key: fs.readFileSync(path.resolve(__dirname, 'ssl/server.key')), 
+cert: fs.readFileSync(path.resolve(__dirname, 'ssl/server.crt')) 
+}
+
+const host = "127.0.0.1";
+const port = "2010";
+console.log("https://"+host+":"+port);
+const server = require('https').createServer(option, function (req, res) {
+
+switch(req.method){
+case "GET":
+console.log(req.method);
+switch(req.url){
+case "/":
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('/'); //write a response
+res.end(); //end the response
+break;
+case "/json":
+res.writeHead(200, {"Content-Type": "application/json"}); 
+var otherArray = ["item1", "item2"];  
+var otherObject = { item1: "item1val", item2: "item2val" }; 
+var json = JSON.stringify({ 
+anObject: otherObject, 
+anArray: otherArray, 
+another: "item" 
+});
+res.write(json); //write a response
+res.end(); //end the response
+break;
+};
+break;
+case "POST":
+console.log(req.method);
+break;
+};
+
+});
+server.listen(port, host);
